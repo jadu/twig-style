a7002469bf2cdc049ce18bdebdc7b3e66ddb1885
Christopher Pettitt <29281467+christopherp309@users.noreply.github.com>
2024-02-02 12:19:01 +0000

    Add block name spacing and operator name spacing rules to Jadu standard

--- src/Standard/JaduStandard.php
+++ src/Standard/JaduStandard.php
@@ -6,7 +6,9 @@ use Jadu\Style\Twig\Rule\Block\BlockNewLineRule;
 use Jadu\Style\Twig\Rule\Block\EndblockNameRule;
 use Jadu\Style\Twig\Rule\Filter\NoSpacelessTagRule;
 use Jadu\Style\Twig\Rule\Punctuation\PunctuationSpacingRule;
+use TwigCsFixer\Rules\Delimiter\BlockNameSpacingRule;
 use TwigCsFixer\Rules\Delimiter\DelimiterSpacingRule;
+use TwigCsFixer\Rules\Operator\OperatorNameSpacingRule;
 use TwigCsFixer\Rules\Operator\OperatorSpacingRule;
 use TwigCsFixer\Rules\Punctuation\TrailingCommaSingleLineRule;
 use TwigCsFixer\Rules\RuleInterface;
@@ -25,12 +27,14 @@ class JaduStandard implements StandardInterface
     {
         return [
             new BlankEOFRule(),
+            new BlockNameSpacingRule(),
             new BlockNewLineRule(),
             new DelimiterSpacingRule(),
             new EmptyLinesRule(),
             new EndblockNameRule(),
             new IndentRule(),
             new NoSpacelessTagRule(),
+            new OperatorNameSpacingRule(),
             new OperatorSpacingRule(),
             new PunctuationSpacingRule(),
             new TrailingCommaSingleLineRule(),
